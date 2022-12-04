3ad74c0d0626587f0956a3a641e9ffe087a29c15
Nikhildyaga <84328401+Nikhildyaga@users.noreply.github.com>
2022-12-04 19:46:38 +0530

    Create middle element in linked lsit.cpp

---
+++ geeksforgeeks/middle element in linked lsit.cpp
@@ -0,0 +1,91 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+void printList(Node* node) 
+{ 
+    while (node != NULL) { 
+        cout << node->data <<" "; 
+        node = node->next; 
+    }  
+    cout<<"\n";
+} 
+
+
+// } Driver Code Ends
+/* Link list Node 
+struct Node {
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+}; */
+class Solution{
+    public:
+    /* Should return data of middle node. If linked list is empty, then  -1*/
+    int getMiddle(Node *head)
+    {
+        // Your code here
+        Node* temp=head;
+        int c=0;
+        while(temp!=NULL)
+        {
+            temp=temp->next;
+            c++;
+        }
+        if(c&1) c=(c+1)/2;
+        else c=(c+2)/2;
+        temp=head;
+        while(c!=1)
+        {
+            temp=temp->next;
+            c--;
+        }
+        return temp->data;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    //code
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int data;
+        cin>>data;
+        struct Node *head = new Node(data);
+        struct Node *tail = head;
+        for (int i = 0; i < N-1; ++i)
+        {
+            cin>>data;
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+        
+        Solution ob;
+        cout << ob.getMiddle(head) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
